b05eec956361620ea2220cf868d8ed925c390359
jongyun <cjy50332@gmail.com>
2024-08-01 13:56:00 +0900

    first changed

---
+++ KDT_ICN/Lecture1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    
+    a = 123.45;
+    b = 200;
+    
+    cout <<"a의 값은 " << a << " 이다" << endl;
+    cout <<"b의 값은 " << b << " 이다" << endl;
+    
+    return 0;
+}
