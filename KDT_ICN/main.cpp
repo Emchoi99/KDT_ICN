83cb5a20642d0767bdfebc2ddaa27144e7882c72
jongyun <cjy50332@gmail.com>
2024-08-01 13:51:21 +0900

    Initial Commit

---
+++ KDT_ICN/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KDT_ICN
+//
+//  Created by 최종윤 on 8/1/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
