519ff56ea5443ec71b4640eb50c5be61753d2438
jongyun <cjy50332@gmail.com>
2024-08-01 13:58:22 +0900

    second changed

--- KDT_ICN/Lecture1.cpp
+++ KDT_ICN/Lecture1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main()
@@ -11,6 +12,8 @@ int main()
     
     cout <<"a의 값은 " << a << " 이다" << endl;
     cout <<"b의 값은 " << b << " 이다" << endl;
+    printf("a의 값은 %d이다\n",a);
+    printf("b의 값은 %f이다\n",b);
     
     return 0;
 }
